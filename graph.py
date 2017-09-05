ba4fcfbd0d25ddec5751044efac285c0373a6a75
JeffryMok <31594606+JeffryMok@users.noreply.github.com>
2017-09-05 12:06:10 +0700

    Add files via upload

--- graph.py
+++ graph.py
@@ -16,26 +16,29 @@ G=nx.Graph()
 G.add_nodes_from(['a','b','c','d','e','f','g','h'])
 
 
-G.add_edges_from([("a","b"),("a","c"),("a","d"),("a","f"),("a","g"),("a","h")],color='b')
+G.add_edges_from([('a','b'),('a','c'),('a','d'),('a','f'),('a','g'),('a','h')])
 
-G.add_edges_from([("b","c"),("b","d"),("b","e"),("b","f"),("b","g"),("b","h")],color='b')
+G.add_edges_from([('b','c'),('b','d'),('b','e'),('b','f'),('b','g'),('b','h')])
 
-G.add_edges_from([("c","d"),("c","e"),("c","g")],color='b')
+G.add_edges_from([('c','d'),('c','e'),('c','g')])
 
-G.add_edges_from([("d","e"),("d","f"),("d","g"),("d","h")],color='b')
+G.add_edges_from([('d','e'),('d','f'),('d','g'),('d','h')])
 
-G.add_edges_from([("e","f"),("e","g"),("e","h")],color='b')
+G.add_edges_from([('e','f'),('e','g'),('e','h')])
 
-G.add_edges_from([("f","g"),("f","h"),("g","h")],color='b')
+G.add_edges_from([('f','g'),('f','h'),('g','h')])
 
-G.add_edge("a","e",color='g')
+G.add_edge('a','e')
 
-G.add_edge("c","f",color='g')
+G.add_edge('c','f')
 
-G.add_edge("c","h",color='g')
+G.add_edge('c','h')
 
-nx.draw(G, edge_color='b')
+pos=nx.spring_layout(G)
 
+nx.draw_networkx_nodes(G, pos)
+nx.draw_networkx_edges(G,pos,edgelist=[('a','b'),('a','c'),('a','d'),('a','f'),('a','g'),('a','h'),('b','c'),('b','d'),('b','e'),('b','f'),('b','g'),('b','h'),('c','d'),('c','e'),('c','g'),('d','e'),('d','f'),('d','g'),('d','h'),('e','f'),('e','g'),('e','h'),('f','g'),('f','h'),('g','h')],edge_color='b',width=2)
+nx.draw_networkx_edges(G,pos,edgelist=[('a','e'),('c','f'),('c','h')],edge_color='g',width=4)
 
 plt.savefig("graph.png")
 
