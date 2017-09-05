f2ca1c0138429c9f3b456da2d68ff052cded5db1
JeffryMok <31594606+JeffryMok@users.noreply.github.com>
2017-09-05 16:49:10 +0700

    Add files via upload

--- graph.py
+++ graph.py
@@ -1,19 +1,26 @@
 try:
 
   import matplotlib.pyplot as plt
-
+	
 except:
 
   raise 
 import networkx as nx
-
+import math
 
 
 
 G=nx.Graph()
+plt.axis('equal');
 
-
-G.add_nodes_from(['a','b','c','d','e','f','g','h'])
+#G.add_nodes_from(['a','b','c','d','e','f','g','h'])
+nodes = ['a','b','c','d','e','f','g','h']
+angle = 0
+radius = 1
+for n in nodes:
+	G.add_node(n, pos=(math.cos(angle) * radius, math.sin(angle) * radius), name=n)
+	print (math.cos(angle) * radius, math.sin(angle) * radius)
+	angle += (2 * math.pi) / (len(nodes))
 
 
 G.add_edges_from([('a','b'),('a','c'),('a','d'),('a','f'),('a','g'),('a','h')])
@@ -34,12 +41,11 @@ G.add_edge('c','f')
 
 G.add_edge('c','h')
 
-pos=nx.spring_layout(G)
-
-nx.draw_networkx_nodes(G, pos)
-nx.draw_networkx_edges(G,pos,edgelist=[('a','b'),('a','c'),('a','d'),('a','f'),('a','g'),('a','h'),('b','c'),('b','d'),('b','e'),('b','f'),('b','g'),('b','h'),('c','d'),('c','e'),('c','g'),('d','e'),('d','f'),('d','g'),('d','h'),('e','f'),('e','g'),('e','h'),('f','g'),('f','h'),('g','h')],edge_color='b',width=2)
-nx.draw_networkx_edges(G,pos,edgelist=[('a','e'),('c','f'),('c','h')],edge_color='g',width=4)
-
+nx.draw_networkx_nodes(G, nx.get_node_attributes(G, 'pos'))
+nx.draw_networkx_labels(G, nx.get_node_attributes(G, 'pos'), nx.get_node_attributes(G, 'name'));
+nx.draw_networkx_edges(G,nx.get_node_attributes(G, 'pos'),edgelist=[('a','b'),('a','c'),('a','d'),('a','f'),('a','g'),('a','h'),('b','c'),('b','d'),('b','e'),('b','f'),('b','g'),('b','h'),('c','d'),('c','e'),('c','g'),('d','e'),('d','f'),('d','g'),('d','h'),('e','f'),('e','g'),('e','h'),('f','g'),('f','h'),('g','h')],edge_color='b',width=2)
+nx.draw_networkx_edges(G,nx.get_node_attributes(G, 'pos'),edgelist=[('a','e'),('c','f'),('c','h')],edge_color='g',width=4)
+#nx.draw(G, nx.get_node_attributes(G, 'pos'), with_labels=True, node_size=0)
 plt.savefig("graph.png")
 
 plt.show()
